31e6bf1a05ae841324a2a95f8821ee6952012806
unknown <g031k029@ISWNI197.ad.iwate-pu.ac.jp>
2013-11-12 12:07:50 +0900

    com

---
+++ app/Controller/MashupsController.php
@@ -0,0 +1,10 @@
+<?php
+	class MashupsController extends AppController{
+		public $name = "Mashup";
+		public $components = array('DebugKit.Toolbar');
+		public function index(){
+			$result = $this->Mashup->api();
+			$this->set("result",$result);
+		}
+	}
+?>
